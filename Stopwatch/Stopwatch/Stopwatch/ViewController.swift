ade50c1d9aeac3f0da0dab41d677ec83f369d383
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-03-19 21:05:00 -0500

    Edited layout

--- Stopwatch/Stopwatch/Stopwatch/ViewController.swift
+++ Stopwatch/Stopwatch/Stopwatch/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   var timer = NSTimer()
+   
+   var count = 0
+   
+   func result(){
+      count++
+      
+      println(count)
+   }
+   
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
+      timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
    }
 
    override func didReceiveMemoryWarning() {
