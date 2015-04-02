971b9260fce8b88b45833d88e0450ddfb138a67e
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-04-01 23:11:38 -0400

    started multiplication table app

---
+++ Multiplication Tables/Multiplication Tables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiplication Tables
+//
+//  Created by Pradyuman Vig on 4/1/15.
+//  Copyright (c) 2015 Asuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
